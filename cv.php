f2b2a4f5e6c01496287e3dfaba4d8589bc849e4f
Sean Scott <sean@icansee.co.uk>
2019-11-10 23:47:50 +0000

    CV Page

--- cv.php
+++ cv.php
@@ -6,11 +6,153 @@
   <?php include 'template-parts/global/head.php' ?>
   <?php include 'template-parts/global/header.php' ?>
 <main id="top">
-  <section class=" first-section_padding">
+  <section class="cv-opening_section first-section_padding">
+    <div class="flex-container flex-column">
+      <h2>Sean Scott</h2>
+      <h3>Web Development</h3>
+      <div class="contact-container">
+        <h4>
+          <i class="fas fa-fw fa-envelope" title="email"></i>
+          <a href="mailto:scottseank64@gmail.com">scottseank64@gmail.com</a>
+        </h4>
+        <h4>
+          <i class="fab fa-fw fa-linkedin-in"></i>
+          <a href="https://www.linkedin.com/in/sean-scott-154bb6197/" target="_blank" title="Linkedin">LinkedIn</a>
+        </h4>
+        <h4>
+          <i class="fab fa-fw fa-github"></i>
+          <a href="https://github.com/sean-k-scott" target="_blank" title="Github">Github</a>
+        <h4>
+        <h4>
+          <i class="fab fa-fw fa-twitter"></i>
+          <a href="https://twitter.com/seanscott64" target="_blank" title="Twitter">Twitter</a>
+        </h4>
+      </div>
+    </div>
   </section>
-  <section class=" standard-section_padding">
+  <section class="cv-technical_and_personal standard-section_padding">
+    <div class="flex-container flex-column">
+      <div class="content-container">
+        <h3><strong>Technical and Personal Abilities</strong></h3>
+        <h4>Technical</h4>
+        <p>I currently specialise in creating bespoke wordpress themes, using a mixture of HTML5, CSS3, Javascript, Jquery, and PHP alongside Wordpress.</p>
+        <p>I am working on developing my skillset with react.js and gatsby.js to aid me to deliver lightning fast websites and webapps.</p>
+        <h4>Personal</h4>
+        <p>I am a confident, quick learner who prides myself on being helpful, friendly and willing to help others.</p>
+        <p>I am experienced in working within a team, and enjoy this aspect of working life.</p>
+        <p>I have some small experience in project management.</p>
+        <p>I am quick to adapt to new technologies and willing to learn to do things in different ways.</p>
+        <p>I involve myself in the tech industry and keep myself updated with the constant changes of the industry.</p>
+      </div>
+    </div>
   </section>
-  <section class=" last-section_padding">
+  <section class="cv-professional_experience standard-section_padding">
+    <div class="flex-container flex-column">
+      <div class="job-experience">
+        <h3><strong>Professional Experience</strong></h3>
+        <h4>Dunkenhalgh Hotel and Spa</h4>
+        <h5>Bartender &amp; Waiting Staff</h5>
+        <h6>September 2005 - September 2007 (Part Time)</h6>
+        <p>In my part-time role at the Dunkenhalgh hotel whilst attending College, I learnt many important skills that I would take with me into future workplaces:</p>
+        <ul>
+          <li>- Time Management</li>
+          <li>- Dealing with High-pressure situations</li>
+          <li>- Working as part of a team</li>
+          <li>- Customer service</li>
+          <li>- Prioritization</li>
+          <li>- Attention to Detail<li>
+          </ul>
+          <p>These skills would help me prepare for full-time work after College.</p>
+      </div>
+      <div class="job-experience">
+        <h4>Westholme School</h4>
+        <h5>Trainee IT Technician</h5>
+        <h6>September 2007 - September 2009</h6>
+        <p>In my role as Trainee technician, working in a small team I was tasked with assisting in the day-to-day running of the schools IT equipment. This required interacting with both Faculty and Students, as well as driving between three seperate school sites.</p>
+        <h5>Senior IT Technician</h5>
+        <h6>September 2009 - September 2015</h6>
+        <p>In my role as Senior Technician at Westholme, I was required to work with the Network Manager to manage a small team, managing and distributing a workload between myself and other members of our team.</p>
+        <h5>Senior IT Technician &amp; Theatre Technician</h5>
+        <h6>September 2015 - September 2017</h6>
+        <p>In my extra role of Theatre technician, I had additional responsibilities including:</p>
+          <ul>
+            <li>- Operating the Sound and Lighting equipment<li>
+            <li>- Assisting in the running of Theatre Examinations</li>
+            <li>- Practicing safety whilst working at heights</li>
+            <li>- Liasing with external companies hiring the theatre</li>
+          </ul>
+          <p>I was also required to assist in the runing of external hire events, solely representing Westholme at these events.</p>
+          <p>Finally, I Project Managed an installation of a theatre upgrade including new lighting equipment and lighting board.</p>
+      </div>
+      <div class="job-experience">
+        <h4>Freelance</h4>
+        <h5>Theatre Lighting and Sound Technician</h5>
+        <h6>September 2017 - March 2019</h6>
+        <p>Throughout most of my time at University I worked as a Freelance Lighting and Sound Technician. This required learning and utilising new skills such as self-marketing, determination and flexibility.</p>
+      </div>
+      <div class="job-experience">
+        <h4>Currys PC World</h4>
+        <h5>Knowhow Customer Service</h5>
+        <h6>September 2018 - February 2019 (Part Time)</h6>
+        <p>During my Second Year at University, I worked in the Knowhow department at Currys PC World.</p>
+        <p>I had to deal with Customer Issues and complaints, fix both simple and complex computing issues, as well as work in the other departments such as Warehouse, main checkout and sales.</p>
+      </div>
+      <div class="job-experience">
+        <h4>Icansee Digital Agency</h4>
+        <h5>Junior Front End Developer</h5>
+        <h6>March 2019 - Present</h6>
+        <p>Working as a Junior Front End Developer at icansee has allowed me to rapidly develop my skills in HTML, CSS &amp; Sass, Javascript, PHP and GitHub, whilst teaching me new skills such as creating bespoke themes for Wordpress.</p>
+        <p>As part of a team of 4, we regularly work with other departments such as the back end developers, marketing and content creators, and designers to provide our clients with a full digital experience.</p>
+        <p>We also work closely with clients, being as flexible as possible in order to quickly create clean, efficient websites that our clients want.</p>
+        <p>I am also improving my skills in improving website speed, security and hosting daily.</p>
+      </div>
+    </div>
+  </section>
+  <section class="cv-education_history standard-section_padding">
+    <div class="flex-container flex-column">
+      <div class="education-container">
+        <h3><strong>Education</strong></h3>
+        <h4>Manchester Metropolitan University</h4>
+        <h5>BSc (Hons) Web Development</h5>
+        <h6>September 2017 - Present</h6>
+        <p>I am currently studying the following modules:</p>
+        <ul>
+          <li>Y3 - Agency Ready</li>
+          <li>Y3 - User Experience Design</li>
+          <li>Y3 - Practical Web Project</li>
+          <li>Y3 - Systems, Thinking and Organisational Resources</li>
+          <br />
+          <li>Y2 - Web Site Development</li>
+          <li>Y2 - Web Programming</li>
+          <li>Y2 - Digital Project Management</li>
+          <li>Y2 - Creative Media Asset Production</li>
+          <br />
+          <li>Y1 - Web Technologies</li>
+          <li>Y1 - Media Making</li>
+          <li>Y1 - People, Communications and Media Industries</li>
+          <li>Y1 - Information, Systems and Organisational Life</li>
+        </ul>
+      </div>
+      <div class="education-container">
+        <h4>Blackburn College</h4>
+        <h5>Level 3 NVQ in Computer Networking</h5>
+        <h6>September 2005 - September 2007</h6>
+      </div>
+      <div class="education-container">
+        <h4>Moorhead High School</h4>
+        <h5>Secondary Education</h5>
+        <h6>September 2000 - September 2005</h6>
+        <p>11 GCSEs (A* - C Grade)</p>
+      </div>
+    </div>
+  </section>
+  <section class="cv-references_available last-section-padding">
+    <div class="flex-container flex-column">
+      <div class="content-container">
+        <h3><strong>References</strong></h3>
+        <p>References and/or letters of recommendation available on request.</p>
+      </div>
+    </div>
   </section>
 </main>
   <?php include 'template-parts/global/footer.php' ?>
