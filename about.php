3f589c8e03d57a8447faac4f83f1f700d5bd8675
Sean Scott <sean@icansee.co.uk>
2019-11-11 01:07:31 +0000

    about page

---
+++ about.php
@@ -0,0 +1,80 @@
+<!-- Start Flexible Meta and Title Content variables -->
+<?php $metadesc = "Sean Scott is an aspiring front end developer in his third year at MMU studying website development. About Sean is the place to learn more about Sean personally and professionally." ?>
+<?php $metakeywords = "Sean, Scott, web, website, development, front, end, front-end, about" ?>
+<?php $title = "Sean Scott - About - Lancashire Based Front End Web Developer" ?>
+<!-- End Flexible Meta and Title Content variables -->
+  <?php include 'template-parts/global/head.php' ?>
+  <?php include 'template-parts/global/header.php' ?>
+<main id="top">
+  <section class="about-opening_section first-section_padding">
+    <div class="flex-container flex-column">
+      <div class="content-container">
+        <h2><strong>About</strong></h2>
+        <h3>A front end developer specialising in <strong>bespoke wordpress themes</strong> aesthetically refined and highly tailored to <strong>suit you and your business</strong></h3>
+        <p>From Lancashire in the North-West of England, having a love of computers and technology from a young age, I originally started out training to be a Network Engineer.</p>
+        <p>On top of my Networking experience, I tapped into my creative side and learnt to program and operate lighting and sound for Theatre Productions.</p>
+        <p>This led to me deciding to return to University as a mature student, retraining myself in website development.</p>
+        <p>I've spent the past 9 months working with <a href="https://www.icansee.co.uk" target="_blank" ref="nofollow">icansee digital agency</a> creating bespoke wordpress themes for clients, enjoying every moment.</p>
+        <p>I'm continuing to grow and improve my abilities in my professional life, improving my abilty to write clean and efficient code and provide clients with quick loading, responsive and attractive sites.</p>
+      </div>
+    </div>
+  </section>
+  <section class="about-faq_section standard-section_padding">
+    <div class="flex-container flex-column">
+      <div class="content-container">
+        <h2><strong>FAQ</strong></h2>
+        <div class="collapsed row1">
+          <h3>How long have you been a developer?</h3>
+          <span class="open-close"></span>
+        </div>
+        <div class="content1">
+          <p>I have only been a devloper for a short time, around 9 months as of writing this (Nov 2019). I have over 10 years of working experience in IT picking up a variety of cross-applicable skills however.</p>
+        </div>
+          <div class="collapsed row2">
+          <h3>Why did you decide to become a developer?</h3>
+          <span class="open-close"></span>
+        </div>
+          <div class="content2">
+          <p>After working in IT Networking, and further along working in Theatre Lighting and Sound Production, I made the decision to retrain to become a developer, because I wanted to combine my IT knowledge and interest with my Theatre creativity on a daily basis.</p>
+        </div>
+          <div class="collapsed row3">
+            <h3>Why full-time over part-time university?</h3>
+          <span class="open-close"></span>
+        </div>
+          <div class="content3">
+            <p>A variety of factors, I didn't have any long term monetary commitments (renting instead of mortgage, car lease etc), and I had a readily available part time job in my freelance theatre work to supplement my income. I also decided that focusing my energies into a shorter amount of time would allow me to start my new career quicker.</p>
+        </div>
+          <div class="collapsed row4">
+            <h3>Do you recommend university as a mature student?</h3>
+          <span class="open-close"></span>
+        </div>
+        <div class="content4">
+          <p>Absolutely! It takes some adjusting to, especially if you haven't been in education for a while, but it allows you to have focus, guidance and clear goals when learning a new skill or planning a career change which is invaluable. You are also more certain about what you want out of the experience.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="about-closing_section last-section_padding">
+    <div class="flex-container flex-column">
+      <div class="content-container">
+        <h2><strong>Interests and Hobbies</strong></h2>
+        <h3>Interests</h3>
+        <ul>
+          <li>- Responsive Web Design</li>
+          <li>- Large Scale CSS architecture</li>
+          <li>- Content Management Systems</li>
+          <li>- Web Performance</li>
+          <li>- New Technologies</li>
+          <li>- CDN/Cloud based delivery</li>
+        </ul>
+        <h3>Hobbies</h3>
+        <p>I am an ex semi-professional video game player, a hobby which required a lot of skills that make me successful in the workplace, a competitive nature and drive to be at the top of my chosen field, working exceptionally well in a team, dedication and structured practice.</p>
+        <p>I also enjoy theatre productions, video and photo editing, reading, football and golf.</p>
+      </div>
+    </div>
+  </section>
+</main>
+  <?php include 'template-parts/global/footer.php' ?>
+  <?php include 'template-parts/global/slideout.php' ?>
+</body>
+</html>
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,4 +9,24 @@ jQuery( function( $ ) {
 		$( 'body' ).toggleClass( 'menu-open' );
 	});
 
+	$( '.row1' ).click( function() {
+  $( '.row1' ).toggleClass( 'collapsed' );
+    $( '.content1' ).toggleClass( 'collapse' );
+	});
+
+$( '.row2' ).click( function() {
+  $( '.row2' ).toggleClass( 'collapsed' );
+    $( '.content2' ).toggleClass( 'collapse' );
+	});
+
+$( '.row3' ).click( function() {
+  $( '.row3' ).toggleClass( 'collapsed' );
+    $( '.content3' ).toggleClass( 'collapse' );
+	});
+
+$( '.row4' ).click( function() {
+  $( '.row4' ).toggleClass( 'collapsed' );
+    $( '.content4' ).toggleClass( 'collapse' );
+	});
+
 });
