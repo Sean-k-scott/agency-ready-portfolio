2f2d3502f54eb0b4377c72f45d92948281f74ed9
Sean Scott <sean@icansee.co.uk>
2019-11-11 22:13:59 +0000

    defer jquery and fontawesome

---
+++ blog.php
@@ -0,0 +1,38 @@
+<!-- Start Flexible Meta and Title Content variables -->
+<?php $metadesc = "Sean Scott is an aspiring front end developer in his third year at MMU studying website development, his blog goes into detail about his place in the industry and the creation of his portfolio site" ?>
+<?php $metakeywords = "Sean, Scott, web, website, development, front, end, front-end, blog" ?>
+<?php $title = "Sean Scott - Blog - Lancashire Based Front End Web Developer" ?>
+<!-- End Flexible Meta and Title Content variables -->
+  <?php include 'template-parts/global/head.php' ?>
+  <?php include 'template-parts/global/header.php' ?>
+<main id="top">
+  <section class="blog-post_one first-section_padding">
+    <div class="flex-container">
+      <div class="content-container">
+        <h2><strong>Blog</strong></h2>
+        <h3> Blog Post One - The Industry</h3>
+        <p>Lorem Ipsum</p>
+      </div>
+    </div>
+  </section>
+  <section class="blog-post_one standard-section_padding">
+    <div class="flex-container">
+      <div class="content-container">
+        <h3> Blog Post Two - Building my Portfolio</h3>
+        <p>Lorem Ipsum</p>
+      </div>
+    </div>
+  </section>
+  <section class="blog-post_one last-section_padding">
+    <div class="flex-container">
+      <div class="content-container">
+        <h3> Blog Post Three - Performance and Optimization</h3>
+        <p>Lorem Ipsum</p>
+      </div>
+    </div>
+  </section>
+</main>
+  <?php include 'template-parts/global/footer.php' ?>
+  <?php include 'template-parts/global/slideout.php' ?>
+</body>
+</html>
--- template-parts/global/head.php
+++ template-parts/global/head.php
@@ -10,7 +10,7 @@
   <link rel="icon" href="/assets/img/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="/assets/css/portfolio.css">
   <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,700%7CProza+Libre:400,600%7CDancing+Script:700&display=swap" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://kit.fontawesome.com/a73609e08e.js" crossorigin="anonymous"></script>
+  <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script defer src="https://kit.fontawesome.com/a73609e08e.js" crossorigin="anonymous"></script>
   <script src="/assets/js/custom.js"></script>
 </head>
