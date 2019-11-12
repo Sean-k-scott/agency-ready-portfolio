42b4c782b0b2d0ff8f5ea9743d066ad8f2cbcebf
Sean Scott <sean@icansee.co.uk>
2019-11-12 14:22:05 +0000

    final changes

--- portfolio.php
+++ portfolio.php
@@ -9,7 +9,6 @@
   <section class="portfolio-opening_section first-section_padding">
     <div class="flex-container flex-column">
       <h2><strong>Below are some of the sites I've worked on in the past year both myself and for icansee:</strong></h2>
-      <h3><a href="#developed">Developed (D)</a> - <a href="#worked-on">Worked On (W)</a> - <a href="#upcoming">Upcoming (U)</a>.</h3>
     </div>
   </section>
   <section id="developed" class="portfolio-case_study standard-section_padding">
@@ -18,7 +17,7 @@
         <img src="/assets/img/portfolio/sovereign-education.jpg" alt="Screenshot of the Sovereign Education homepage" />
       </div>
       <div class="content-right">
-        <h2><strong>Sovereign Education (D)</strong></h2>
+        <h2><strong>Sovereign Education</strong></h2>
         <p>Sovereign Education offer effective A-Level study days at locations across the UK to inspire both students and teachers.</p>
         <p>Sovereign Education required a new site for the 2019-20 academic year to advertise their courses, provide further information on their courses and book a course online.</p>
         <div>Technologies Used:
@@ -40,7 +39,7 @@
         <img src="/assets/img/portfolio/agency-ready-portfolio-site.jpg" alt="Screenshot of this website - Agency Ready Portfolio" />
       </div>
       <div class="content-left">
-        <h2><strong>Agency Ready Portfolio (D)</strong></h2>
+        <h2><strong>Agency Ready Portfolio</strong></h2>
         <p>This website! This site was created for the Agency Ready module of my 3rd year studies at Manchester Metropolitan University (MMU). Created without a framework or back-end, this site was created as accessibly as possible to display my previous work and give more information about myself.</p>
         <div>Technologies Used:
           <ul>
@@ -59,7 +58,7 @@
         <img src="/assets/img/portfolio/get-involved.jpg" alt="Screenshot of the Get Involved homepage" />
       </div>
       <div class="content-right">
-        <h2><strong>Get Involved (D)</strong></h2>
+        <h2><strong>Get Involved</strong></h2>
         <p>Created for my Digital Project Management module in Year 2 of my Degree, Get Involved was created for our charity company in order for more people to get involved with their local charities.</p>
         <p>Get Involved was my first proper attempt at a website, and putting what I'd learnt of HTML/CSS/Flex into use. As the site was part of a Project Management module the site itself wasn't the main focus, and could definitely have been developed further.</p>
         <div>Technologies Used:
@@ -79,7 +78,7 @@
         <img src="/assets/img/portfolio/supalite-microsite.jpg" alt="Screenshot of Supalite Newcastle homepage" />
       </div>
       <div class="content-left">
-        <h2><strong>Supalite Newcastle Microsite (D)</strong></h2>
+        <h2><strong>Supalite Newcastle Microsite</strong></h2>
         <p>A microsite comissioned by Supalite to advertise Enhance Conservatories work in Newcastle, this microsite was created in a short timeframe.</p>
         <div>Technologies Used:
           <ul>
@@ -99,7 +98,7 @@
         <img src="/assets/img/portfolio/leon-atkins.jpg" alt="Screenshot of the Leon Atkins homepage" />
       </div>
       <div class="content-right">
-        <h2><strong>Leon Atkins (W)</strong></h2>
+        <h2><strong>Leon Atkins</strong></h2>
         <p>Leon Atkins' design &amp; install stunning practical decking solutions across the UK.</p>
         <p>Leon required a website to advertise his business, for people to see what types of decking he provides and to get in touch with him.</p>
         <div>Technologies Used:
@@ -121,7 +120,7 @@
         <img src="/assets/img/portfolio/canterbury-fireplaces.jpg" alt="Screenshot of Canterbury Fireplaces homepage" />
       </div>
       <div class="content-left">
-        <h2><strong>Canterbury Fireplaces (W)</strong></h2>
+        <h2><strong>Canterbury Fireplaces</strong></h2>
         <p>Canterbury Fireplaces is a friendly, family run company that puts their customers' needs first. Their 4000 square foot showroom in Blackburn is home to over 150 fires and fire surrounds, with more than 100 live displays.</p>
         <div>Technologies Used:
           <ul>
@@ -142,7 +141,7 @@
         <img src="/assets/img/portfolio/beechs-butchers.jpg" alt="Screenshot of the upcoming Beechs Butchers homepage" />
       </div>
       <div class="content-right">
-        <h2><strong>Beech's Butchers (U)</strong></h2>
+        <h2><strong>Beech's Butchers (Upcoming)</strong></h2>
         <p>Beech's Butchers provides the finest locally sourced produce. Beech's have three shops located in Wilpshire, Barrowford and Bamford Farm Shop.</p>
         <p>John wanted an updated site to go with the newly opened Bamford Farm Shop. You can order through 'Click &amp; Collect' on the site, and your order will be prepared for you to collect in-store.</p>
         <div>Technologies Used:
@@ -164,7 +163,7 @@
         <img src="/assets/img/portfolio/supalite-newtheme.jpg" alt="Screenshot of the Supalite new theme homepage" />
       </div>
       <div class="content-left">
-        <h2><strong>Supalite Roof (U)</strong></h2>
+        <h2><strong>Supalite Roof (Upcoming)</strong></h2>
         <p>Supalite transform conservatories into year round living spaces.</p>
         <p>An updated theme for the main Supaliteroof website, the new responsive theme more closely reflects the companies brochure.</p>
         <div>Technologies Used:
